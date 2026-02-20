55bfa76d79d1350c8bca8806510ba33f4b768830
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-02-20 16:11:37 +0400

    feature/add missing pages

--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,14 @@ import {
 import Home from "./pages/Home.tsx";
 import Settings from "./pages/Settings.tsx";
 import App from "./App.tsx";
-import Team from "./pages/Team.tsx";
 import Login from "./pages/Login.tsx";
 import Registration from "./pages/Registration.tsx";
 import Requests from "./pages/Requests.tsx";
-import Bookings from "./pages/Bookings.tsx";
 import Analytics from "./pages/Analytics.tsx";
-import Profile from "./pages/Profile.tsx";
+import Staff from "./pages/Staff.tsx";
+import Departments from "./pages/Departments.tsx";
+import Shifts from "./pages/Shifts.tsx";
+import Integration from "./pages/Integration.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,12 +27,13 @@ const router = createBrowserRouter(
       <Route path="/registration" element={<Registration />} />
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
+        <Route path="/staff" element={<Staff />} />
+        <Route path="/departments" element={<Departments />} />
+        <Route path="/shifts" element={<Shifts />} />
         <Route path="/requests" element={<Requests />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/team" element={<Team />} />
         <Route path="/analytics" element={<Analytics />} />
+        <Route path="/integration" element={<Integration />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/profile" element={<Profile />} />
       </Route>
     </>,
   ),
--- src/pages/Team.tsx
+++
@@ -1,5 +0,0 @@
-const Team = () => {
-  return <div className="max-w-7xl mx-auto flex flex-col gap-8">Team</div>;
-};
-
-export default Team;
