88686d3c5af0809da6cf2fb716168c573eb2d867
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-02-25 12:22:29 +0400

    feature/add analytics page
    - Header;
    - Analytics cards

---
+++ src/constants/analyticsData.tsx
@@ -0,0 +1,140 @@
+export const ANALYTICS_CARDS_DATA = [
+  {
+    id: 1,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-chart-column w-5 h-5"
+      >
+        <path d="M3 3v16a2 2 0 0 0 2 2h16"></path>
+        <path d="M18 17V9"></path>
+        <path d="M13 17V5"></path>
+        <path d="M8 17v-3"></path>
+      </svg>
+    ),
+    title: "totalRequests",
+    analyticsResult: "276",
+  },
+  {
+    id: 2,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="lucide lucide-circle-check w-5 h-5"
+      >
+        <circle cx="12" cy="12" r="10"></circle>
+        <path d="m9 12 2 2 4-4"></path>
+      </svg>
+    ),
+    title: "completed",
+    analyticsResult: "203",
+  },
+  {
+    id: 3,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="lucide lucide-trending-up w-5 h-5"
+      >
+        <polyline points="22 7 13.5 15.5 8.5 10.5 2 17"></polyline>
+        <polyline points="16 7 22 7 22 13"></polyline>
+      </svg>
+    ),
+    title: "active",
+    analyticsResult: "73",
+  },
+  {
+    id: 4,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="lucide lucide-clock w-5 h-5"
+      >
+        <circle cx="12" cy="12" r="10"></circle>
+        <polyline points="12 6 12 12 16 14"></polyline>
+      </svg>
+    ),
+    title: "avgTime",
+    analyticsResult: "12m",
+  },
+  {
+    id: 5,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="lucide lucide-building2 w-5 h-5"
+      >
+        <path d="M6 22V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v18Z"></path>
+        <path d="M6 12H4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h2"></path>
+        <path d="M18 9h2a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2h-2"></path>
+        <path d="M10 6h4"></path>
+        <path d="M10 10h4"></path>
+        <path d="M10 14h4"></path>
+        <path d="M10 18h4"></path>
+      </svg>
+    ),
+    title: "mostLoaded",
+    analyticsResult: "Housekeeping",
+  },
+  {
+    id: 6,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="lucide lucide-zap w-5 h-5"
+      >
+        <path d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"></path>
+      </svg>
+    ),
+    title: "peakHour",
+    analyticsResult: "10:00-11:00",
+  },
+];
--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,5 +1,13 @@
+import AnalyticsCards from "../components/main/analytics/AnalyticsCards";
+import AnalyticsHeader from "../components/main/analytics/AnalyticsHeader";
+
 const Analytics = () => {
-  return <div>Analytics</div>;
+  return (
+    <div className="max-w-7xl mx-auto flex flex-col gap-8">
+      <AnalyticsHeader />
+      <AnalyticsCards />
+    </div>
+  );
 };
 
 export default Analytics;
