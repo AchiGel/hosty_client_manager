b6d3c6eed553c4a16ba3fe51a394614c103b0e59
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-02-26 16:24:59 +0400

    hot-fix/minor styling changes in ServiceItem

--- src/components/main/settings/ServiceItem.tsx
+++ src/components/main/settings/ServiceItem.tsx
@@ -20,7 +20,9 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
   return (
     <div className="p-4 flex items-center justify-between hover:bg-gray-50 transition-colors">
       <div className="flex items-center gap-4">
-        <div className="p-2.5 rounded-lg bg-[#c5a667]/10 text-[#c5a667]">
+        <div
+          className={`p-2.5 rounded-lg bg-[#c5a667]/10 ${isActive ? "text-[#c5a667]" : "text-gray-400"} `}
+        >
           {children}
         </div>
         <div>
@@ -35,11 +37,11 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
         data-state={isActive ? "checked" : "unchecked"}
         value="on"
         onClick={() => onToggle(id)}
-        className="peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors data-[state=unchecked]:bg-input focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-[#c5a667]"
+        className="peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors data-[state=unchecked]:bg-[#dcdfe5] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-[#f6f7f9] disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-[#c5a667]"
       >
         <span
           data-state={isActive ? "checked" : "unchecked"}
-          className="pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+          className="pointer-events-none block h-5 w-5 rounded-full bg-[#f6f7f9] shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
         ></span>
       </button>
     </div>
