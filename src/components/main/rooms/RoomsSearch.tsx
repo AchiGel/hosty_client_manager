c8a720ccb6a7e9256d5ac249ff8a1ddeb9472536
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-03-13 14:54:19 +0400

    feature/add radix select to rooms search

--- src/components/main/rooms/RoomsSearch.tsx
+++ src/components/main/rooms/RoomsSearch.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { ChevronDownIcon, SearchIcon } from "../../assets";
+import * as Select from "@radix-ui/react-select";
 import { ROOM_CATEGORIES } from "../../../constants/roomCategories";
 
 const RoomsSearch = () => {
@@ -14,20 +15,47 @@ const RoomsSearch = () => {
           className="flex h-10 w-full rounded-md border border-[#dcdfe5] bg-[#f6f7f9] px-3 py-2 text-base ring-[#f6f7f9] file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#c9a65e] focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm pl-9"
         />
       </div>
-      <button
-        type="button"
-        role="combobox"
-        aria-controls="radix-:r0:"
-        aria-expanded="false"
-        aria-autocomplete="none"
-        dir="ltr"
-        data-state="closed"
-        className="flex h-10 items-center justify-between rounded-md border border-[#dcdfe5] px-3 py-2 text-sm ring-[#f6f7f9] placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&amp;&gt;span]:line-clamp-1 w-full sm:w-48 bg-white"
-      >
-        <span>{t("common.all")}</span>
-        <ChevronDownIcon />
-      </button>
-      <select className="flex h-10 items-center justify-between rounded-md border border-[#dcdfe5] px-3 py-2 text-sm ring-[#f6f7f9] placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&amp;&gt;span]:line-clamp-1 w-full sm:w-48 bg-white"></select>
+      <Select.Root>
+        <Select.Trigger className="cursor-pointer flex h-10 items-center justify-between rounded-md border border-[#dcdfe5] px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-[#c9a65e] focus:ring-offset-2 w-full sm:w-48 bg-white">
+          <Select.Value placeholder={t("common.all")} />
+          <ChevronDownIcon className="ml-2 h-4 w-4 opacity-60" />
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content
+            side="bottom"
+            align="start"
+            className="z-50 mt-1 w-(--radix-select-trigger-width) rounded-md border border-[#dcdfe5] bg-white shadow-md"
+          >
+            <Select.Viewport className="p-1">
+              <Select.Item
+                value="all"
+                className="relative cursor-pointer rounded-sm px-3 py-2 pl-8 text-sm outline-none hover:bg-[#c9a65e] flex items-center"
+              >
+                <Select.ItemIndicator className="absolute left-2 flex items-center">
+                  ✓
+                </Select.ItemIndicator>
+
+                <Select.ItemText>{t("common.all")}</Select.ItemText>
+              </Select.Item>
+
+              {ROOM_CATEGORIES.map((rc) => (
+                <Select.Item
+                  key={rc.roomType}
+                  value={rc.roomType}
+                  className="relative cursor-pointer rounded-sm px-3 py-2 pl-8 text-sm outline-none hover:bg-[#c9a65e] flex items-center"
+                >
+                  <Select.ItemIndicator className="absolute left-2 flex items-center">
+                    ✓
+                  </Select.ItemIndicator>
+
+                  <Select.ItemText>{rc.roomType}</Select.ItemText>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
     </div>
   );
 };
